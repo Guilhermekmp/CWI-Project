7b587004760ef1de1a44dcf6283f7e6ca52728e6
guilherme <guilherme.pires@basis.com.br>
2021-11-24 14:58:15 -0300

    Cria projeto com configurações inicias

---
+++ assembleia-service/src/main/java/com/gov/assembleia/assembleiaservice/AssembleiaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gov.assembleia.assembleiaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaServiceApplication.class, args);
+	}
+
+}
---
+++ assembleia-service/src/test/java/com/gov/assembleia/assembleiaservice/AssembleiaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gov.assembleia.assembleiaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssembleiaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
